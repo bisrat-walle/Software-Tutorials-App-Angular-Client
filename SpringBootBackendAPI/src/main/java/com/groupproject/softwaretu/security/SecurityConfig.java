895b86858f6c88790a655eb58c2f3ce46c835ab8
bisrat-walle <bisratwalle3@gmail.com>
2022-02-11 10:27:26 +0300

    deleting redundant configration

--- SpringBootBackendAPI/src/main/java/com/groupproject/softwaretu/security/SecurityConfig.java
+++ SpringBootBackendAPI/src/main/java/com/groupproject/softwaretu/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
